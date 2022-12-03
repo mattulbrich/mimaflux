7041fa8a116ca155159c3cd86cd4c4f9f1b99fda
Mattias Ulbrich <ulbrich@kit.edu>
2022-12-03 14:42:33 +0100

    improved error reporting

--- src/main/java/edu/kit/kastel/formal/mimaflux/Interpreter.java
+++ src/main/java/edu/kit/kastel/formal/mimaflux/Interpreter.java
@@ -1,13 +1,21 @@
 package edu.kit.kastel.formal.mimaflux;
 
 import edu.kit.kastel.formal.mimaflux.MimaAsmParser.FileContext;
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Map;
 
@@ -29,9 +37,13 @@ public class Interpreter {
         MimaAsmLexer lexer = new MimaAsmLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MimaAsmParser parser = new MimaAsmParser(tokens);
-        BailOutErrorStrategy errorStrategy = new BailOutErrorStrategy();
-        parser.setErrorHandler(errorStrategy);
-        parser.addErrorListener(errorStrategy.ERROR_LISTENER);
+        parser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                throw new RuntimeException("line " + line + ":" +
+                        charPositionInLine + ": " + msg);
+            }
+        });
         FileContext content = parser.file();
         ProgramVisitor pv = new ProgramVisitor();
         content.accept(pv);
